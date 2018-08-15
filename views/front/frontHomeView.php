1442bf45a7aeaa184a33ffa1d1127f1fff2c17e2
nicolas5789 <nicolas5789@gmail.com>
2018-08-16 00:37:47 +0200

    debut design home

--- views/front/frontHomeView.php
+++ views/front/frontHomeView.php
@@ -3,16 +3,158 @@
 <?php ob_start(); ?>
 
 <h1>Hello there !</h1>
-<div id="departements">
-	<a href="index.php?action=listNounous&amp;idDept=95">Val d'Oise</a>
-	<a href="index.php?action=listNounous&amp;idDept=78">Yvelines</a>
-	<a href="index.php?action=listNounous&amp;idDept=92">Hauts de Seine</a>
-	<a href="index.php?action=listNounous&amp;idDept=75">Paris</a>
-	<a href="index.php?action=listNounous&amp;idDept=93">Seine Saint Denis</a>
-	<a href="index.php?action=listNounous&amp;idDept=94">Val de Marne</a>
-	<a href="index.php?action=listNounous&amp;idDept=77">Seine et Marne</a>
-</div>
+<div id="accueil">
+	<div id="departements">
+		<div id="dept_col">
 
+			<div class="dept" id="dept95">
+				<a href="index.php?action=listNounous&amp;idDept=95">
+					<span>Val d'Oise</span>
+					<div class="logo_dept">
+						<img src="public/images/logo95.png">
+					</div>
+					
+				</a>
+			</div>
+			<div class="dept" id="dept78">
+				<a href="index.php?action=listNounous&amp;idDept=78">
+					<span>Yvelines</span>
+					<div class="logo_dept">
+						<img src="public/images/logo78.png">
+					</div>
+					
+				</a>
+			</div>
+			<div class="dept" id="dept92">
+				<a href="index.php?action=listNounous&amp;idDept=92">
+					<span>Hauts de Seine</span>
+					<div class="logo_dept">
+						<img src="public/images/logo92.png">
+					</div>
+					
+				</a>
+			</div>
+			<div class="dept" id="dept75">
+				<a href="index.php?action=listNounous&amp;idDept=75">
+					<span>Paris</span>
+					<div class="logo_dept">
+						<img src="public/images/logo75.png">
+					</div>
+					
+				</a>
+			</div>
+
+			<div class="dept" id="dept93">
+				<a href="index.php?action=listNounous&amp;idDept=93">
+					<span>Seine Saint Denis</span>
+					<div class="logo_dept">
+						<img src="public/images/logo93.png">
+					</div>
+
+					
+				</a>
+			</div>
+			<div class="dept" id="dept94">
+				<a href="index.php?action=listNounous&amp;idDept=94">
+					<span>Val de Marne</span>
+					<div class="logo_dept">
+						<img src="public/images/logo94.png">
+					</div>
+					
+				</a>
+			</div>
+			<div class="dept" id="dept77">
+				<a href="index.php?action=listNounous&amp;idDept=77">
+					<span>Seine et Marne</span>
+					<div class="logo_dept">
+						<img src="public/images/logo77.png">
+					</div>
+					
+				</a>
+			</div>
+
+		</div>
+	</div>
+
+	
+	<div id="notice">
+		<p>Hey !</p>
+	</div>
+
+
+	<div id="departements">
+		<div id="dept_col">
+
+			<div class="dept" id="dept95">
+				<a href="index.php?action=listNounous&amp;idDept=95">
+					<span>Val d'Oise</span>
+					<div class="logo_dept">
+						<img src="public/images/logo95.png">
+					</div>
+					
+				</a>
+			</div>
+			<div class="dept" id="dept78">
+				<a href="index.php?action=listNounous&amp;idDept=78">
+					<span>Yvelines</span>
+					<div class="logo_dept">
+						<img src="public/images/logo78.png">
+					</div>
+					
+				</a>
+			</div>
+			<div class="dept" id="dept92">
+				<a href="index.php?action=listNounous&amp;idDept=92">
+					<span>Hauts de Seine</span>
+					<div class="logo_dept">
+						<img src="public/images/logo92.png">
+					</div>
+					
+				</a>
+			</div>
+			<div class="dept" id="dept75">
+				<a href="index.php?action=listNounous&amp;idDept=75">
+					<span>Paris</span>
+					<div class="logo_dept">
+						<img src="public/images/logo75.png">
+					</div>
+					
+				</a>
+			</div>
+
+			<div class="dept" id="dept93">
+				<a href="index.php?action=listNounous&amp;idDept=93">
+					<span>Seine Saint Denis</span>
+					<div class="logo_dept">
+						<img src="public/images/logo93.png">
+					</div>
+
+					
+				</a>
+			</div>
+			<div class="dept" id="dept94">
+				<a href="index.php?action=listNounous&amp;idDept=94">
+					<span>Val de Marne</span>
+					<div class="logo_dept">
+						<img src="public/images/logo94.png">
+					</div>
+					
+				</a>
+			</div>
+			<div class="dept" id="dept77">
+				<a href="index.php?action=listNounous&amp;idDept=77">
+					<span>Seine et Marne</span>
+					<div class="logo_dept">
+						<img src="public/images/logo77.png">
+					</div>
+					
+				</a>
+			</div>
+
+		</div>
+	</div>
+
+</div>
 <?php $content = ob_get_clean(); ?>
 
 <?php require("views/template.php"); ?>
